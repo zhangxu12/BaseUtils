6648327696d85c78cbe089e5b32ee194e02a4b51
zhangxu12 <2211088929@qq.com>
2018-03-27 15:14:21 +0800

    修改md5加密

--- src/main/java/com/zx/encode/MD5.java
+++ src/main/java/com/zx/encode/MD5.java
@@ -1,39 +1,82 @@
 package com.zx.encode;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MD5 {
-    public final static String md5(String content) {
-        //用于加密的字符
-        char[] md5String = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                'A', 'B', 'C', 'D', 'E', 'F'};
+    /**
+     *
+     * @param plainText
+     *            明文
+     * @return 32位密文
+     */
+    public static String encryption(String plainText) {
+        String re_md5 = null;
         try {
-            //使用平台的默认字符集将此 String 编码为 byte序列，并将结果存储到一个新的 byte数组中
-            byte[] btInput = content.getBytes();
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            try {
+                md.update(plainText.getBytes("Utf-8"));
+            } catch (UnsupportedEncodingException e) {
+               e.printStackTrace();
+            }
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0) {
+                    i += 256;
+                }
+                if (i < 16){
+                    buf.append("0");
+                }
+                buf.append(Integer.toHexString(i));
+            }
+            re_md5 = buf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return re_md5;
+    }
 
-            //信息摘要是安全的单向哈希函数，它接收任意大小的数据，并输出固定长度的哈希值。
+    /**
+     *
+     * @param originstr
+     *            明文
+     * @return 32位密文 大写
+     */
+    public static String encryptionUp(String originstr) {
+
+        //var hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        // update MD5 string to lower-case to adapt to UMP test
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        try
+        {
+            byte[] btInput = originstr.getBytes("UTF-8");
+
             MessageDigest mdInst = MessageDigest.getInstance("MD5");
 
-            //MessageDigest对象通过使用 update方法处理数据， 使用指定的byte数组更新摘要
             mdInst.update(btInput);
 
-            // 摘要更新之后，通过调用digest（）执行哈希计算，获得密文
             byte[] md = mdInst.digest();
 
-            // 把密文转换成十六进制的字符串形式
             int j = md.length;
-            char[] str = new char[j * 2];
+            char str[] = new char[j * 2];
             int k = 0;
-            for (byte byte0 : md) {
-                str[k++] = md5String[byte0 >>> 4 & 0xf];
-                str[k++] = md5String[byte0 & 0xf];
+            for (int i = 0; i < j; i++)
+            {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
             }
-
-            //返回经过加密后的字符串
             return new String(str);
-
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
             return null;
         }
+
     }
 }
