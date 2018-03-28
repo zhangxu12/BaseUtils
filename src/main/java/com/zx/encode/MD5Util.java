22f3d882d3fd94e788030a1ecfc81e9fb54a8761
zhangxu12 <2211088929@qq.com>
2018-03-28 13:25:11 +0800

    修改md5工具类

--- src/main/java/com/zx/encode/MD5Util.java
+++ src/main/java/com/zx/encode/MD5Util.java
@@ -4,17 +4,16 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class MD5 {
+public class MD5Util {
     /**
      *
-     * @param plainText
-     *            明文
+     * @param plainText 明文
      * @return 32位密文
      */
     public static String encryption(String plainText) {
         String re_md5 = null;
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
+            MessageDigest md = MessageDigest.getInstance("MD5Util");
             try {
                 md.update(plainText.getBytes("Utf-8"));
             } catch (UnsupportedEncodingException e) {
@@ -23,12 +22,12 @@ public class MD5 {
             byte b[] = md.digest();
             int i;
             StringBuffer buf = new StringBuffer("");
-            for (int offset = 0; offset < b.length; offset++) {
-                i = b[offset];
+            for (byte aB : b) {
+                i = aB;
                 if (i < 0) {
                     i += 256;
                 }
-                if (i < 16){
+                if (i < 16) {
                     buf.append("0");
                 }
                 buf.append(Integer.toHexString(i));
@@ -42,20 +41,16 @@ public class MD5 {
 
     /**
      *
-     * @param originstr
-     *            明文
+     * @param originstr 明文
      * @return 32位密文 大写
      */
     public static String encryptionUp(String originstr) {
-
-        //var hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-        // update MD5 string to lower-case to adapt to UMP test
         char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         try
         {
             byte[] btInput = originstr.getBytes("UTF-8");
 
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            MessageDigest mdInst = MessageDigest.getInstance("MD5Util");
 
             mdInst.update(btInput);
 
@@ -64,9 +59,7 @@ public class MD5 {
             int j = md.length;
             char str[] = new char[j * 2];
             int k = 0;
-            for (int i = 0; i < j; i++)
-            {
-                byte byte0 = md[i];
+            for (byte byte0 : md) {
                 str[k++] = hexDigits[byte0 >>> 4 & 0xf];
                 str[k++] = hexDigits[byte0 & 0xf];
             }
@@ -78,5 +71,12 @@ public class MD5 {
             return null;
         }
 
+    }
+
+
+    public static void main(String[] args){
+        String str = "zx";
+        System.out.println(MD5Util.encryption(str));
+        System.out.println(MD5Util.encryptionUp(str));
     }
 }
